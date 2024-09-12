805982ad6c09a1231cbb580945b2835dfaf1d825
Brandon Daniel Sánchez Santamaría <brndonsantamaria@gmail.com>
2024-09-12 12:03:03 -0600

    Ya se muestran los datos con la base de datos

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -10,10 +10,9 @@
 //export const IP =  'http://192.168.1.10/MyLoan-new/api';
 //export const IP = 'http://192.168.1.10';
 //colegio lennyn
-export const IP = 'http://192.168.129.108';
 //export const IP = 'http://192.168.137.125'
 //Constante escuela
 //export const IP = 'http://10.10.2.144';
 
-export const IP = 'http://10.10.128.50';
+export const IP = 'http://10.10.2.143';
 
